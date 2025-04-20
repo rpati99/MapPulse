22a4486eabd466b7450995fc1596ed9662ac8a6d
Rachit Prajapati <rdprajapati@icloud.com>
2025-04-19 18:07:39 -0700

    Initial Commit

---
+++ MapPulse/MapPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPulseApp.swift
+//  MapPulse
+//
+//  Created by Rachit Prajapati on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MapPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
